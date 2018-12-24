8aafdedc0ea7fb0e9432c8640669f9532fc081cb
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-24 15:14:50 -0200

    Create Listar os blobs.php

---
+++ Listar os blobs.php
@@ -0,0 +1,18 @@
+<?php
+
+  $listBlobsOptions = new ListBlobsOptions();
+  $listBlobsOptions->setPrefix("HelloWorld");
+
+    echo "These are the blobs present in the container: ";
+
+    do{
+        $result = $blobClient->listBlobs($containerName, $listBlobsOptions);
+        foreach ($result->getBlobs() as $blob)
+        {
+            echo $blob->getName().": ".$blob->getUrl()."<br />";
+        }
+
+        $listBlobsOptions->setContinuationToken($result->getContinuationToken());
+    } while($result->getContinuationToken());
+    
+<?
