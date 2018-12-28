922612aecba9e8314dc4aece78218c4318fe41f4
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-27 23:25:35 -0200

    Update cabecalho.php

--- cabecalho.php
+++ cabecalho.php
@@ -1,6 +1,6 @@
 <html>
 <head lang="pt-br">
-    <title>Azure Storage Managment</title>
+    <title>Azure Storage Management</title>
     <meta charset="utf-8">
     <link href="css/bootstrap.css" rel="stylesheet" />
     <link href="css/loja.css" rel="stylesheet" />
