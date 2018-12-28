8ca82699e6bc288a4e033e9a4cd12b5afb0532bc
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-28 16:16:35 -0200

    Update conexao.php

--- conexao.php
+++ conexao.php
@@ -1,6 +1,6 @@
 <?php
 
-		// PHP Data Objects(PDO) Sample Code:
+// PHP Data Objects(PDO) Sample Code:
 try {
     $conn = new PDO("sqlsrv:server = tcp:databasesiaetn.database.windows.net,1433; Database = sql_SIAETN", "ra805847", "{your_password_here}");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -11,9 +11,8 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ra805847", "pwd" => "ra3025@7610", "Database" => "sql_SIAETN", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "ra805847@databasesiaetn", "pwd" => "ra3025@7610", "Database" => "sql_SIAETN", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:databasesiaetn.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
-
 
 ?>
