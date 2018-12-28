95575dded6d59184b289f49ab26c120836444b88
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-28 07:13:12 -0200

    Update Listar os blobs.php

--- Listar os blobs.php
+++ Listar os blobs.php
@@ -13,11 +13,6 @@
 			<td>".$blob->getName()->nome."</td>
 			<td>".$blob->getUrl()->tamanho."</td>
 			<td>".$arquivo->data."</td>
-			<td>
-				<a href='visualizar_arquivo.php?acao=VISUALIZAR&id_arquivo=".$arquivo->id_arquivo."' target='_blank'><img src='IMG/pdf.png' style='width:30px; height:30px;cursor:pointer;'></a>
-				<a href='visualizar_arquivo.php?acao=DOWNLOAD&id_arquivo=".$arquivo->id_arquivo."' target='_blank'><img src='IMG/download.png' style='width:30px; height:30px;cursor:pointer;'></a>
-				<a href='#' onclick='excluir_arquivo(".$arquivo->id_arquivo.");'><img src='IMG/delete.png' style='width:30px; height:30px;cursor:pointer;'></a>
-			</td>
 		</tr>";
             //echo $blob->getName().": ".$blob->getUrl()."<br />";
         }
