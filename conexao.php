de231cb61564bd27b7d0e91e6d75a617d1b8fd4b
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-28 09:43:46 -0200

    Update Conexao

--- conexao.php
+++ conexao.php
@@ -1,6 +1,19 @@
 <?php
 
-		$conn = new mysqli("localhost", "root", "","documentos");
-		mysqli_set_charset($conn,"utf8");
+		// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:databasesiaetn.database.windows.net,1433; Database = sql_SIAETN", "ra805847", "{your_password_here}");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "ra805847@databasesiaetn", "pwd" => "ra3025@7610", "Database" => "sql_SIAETN", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:databasesiaetn.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
 
 ?>
--- metodos.php
+++ metodos.php
@@ -1,6 +1,5 @@
 <?php
-	$conn = new mysqli("localhost", "root", "","documentos");
-	mysqli_set_charset($conn,"utf8");
+	include("conexao.php")
 
 	error_reporting(0);
 	ini_set("display_errors", 0 );
