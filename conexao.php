96d8cbabb7f5bc887c4b20c9cb5cc8069128c714
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-28 20:59:53 -0200

    Update 201812282059

--- conexao.php
+++ conexao.php
@@ -1,18 +1,18 @@
 <?php
 
-/* PHP Data Objects(PDO) Sample Code:
+// PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:databasesiaetn.database.windows.net,1433; Database = sql_SIAETN", "ra805847", "{your_password_here}");
+    $conn = new PDO("sqlsrv:server = tcp:databasesiaetn.database.windows.net,1433; Database = sql_SIAETN", "ra805847", "ra3025@7610");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
     die(print_r($e));
-}*/
+}
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "ra805847@databasesiaetn", "pwd" => "ra3025@7610", "Database" => "sql_SIAETN", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+/*$connectionInfo = array("UID" => "ra805847@databasesiaetn", "pwd" => "ra3025@7610", "Database" => "sql_SIAETN", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:databasesiaetn.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);
+$conn = sqlsrv_connect($serverName, $connectionInfo);*/
 
 ?>
--- teste.php
+++ teste.php
@@ -1,28 +1,26 @@
 <?php
 
-    //include("conexao.php")
-    try {
-        $db = new PDO("sqlsrv:server = tcp:databasesiaetn.database.windows.net,1433; Database = sql_SIAETN", "ra805847", "ra3025@7610");	
-        
-        
-        $stmt = $db->prepare("Select * From TBSolicitacaoes");
+    include("conexao.php")
+    $sql = "Select 
+            T1.CODIGO,
+            ISNULL(T1.NUM_LICENCA, T1.NUM_PROTOCOLO) as LICENCA,
+            T2.NomeAbreviado,
+            T3.PLCA_VEICULO AS PLCA_CAVALO,
+            T4.PLCA_VEICULO AS PLACA_REBOQUE,
+            T5.SIGLA_REGIAO
+            From
+            dbo.TBSolicitacaoes T1
+            LEFT JOIN dbo.TBCliente T2 ON T2.CODIGO = T1.COD_CLIENTE
+            LEFT JOIN dbo.TBVeiculos T3 ON T3.CODIGO = T1.COD_CAVALO
+            LEFT JOIN dbo.TBVeiculos T4 ON T4.CODIGO = T1.COD_REBOQUE
+            LEFT JOIN dbo.TBRegiao T5 ON T5.CODIGO = T1.COD_REGIAO
+            WHERE T5.SIGLA_REGIAO <> 'NULL'";
+
+        $stmt = $conn->prepare($sql);
         
         
         $stmt->execute();
-        $clientes  = $stmt->fetchAll(PDO::FETCH_OBJ);
         
-        echo '<pre>';
-            print_r($clientes);
-        echo '</pre>';
-    
-    } catch (PDOException $e) {
-        print "Error!: " . $e->getMessage() . "<br/>";
-        die();
-    }
-
-    /*$sql = "Select * From TBSolicitacaoes";
-    $result = sqlsrv_query($conn, $sql);
-
     print "<table>
                 <tr>
                 <th>Licença</th>
@@ -32,13 +30,13 @@
                 <th>Região</th>
                 <th>Ações</th>
             </tr>";
-    while ($arquivo =  sqlsrv_fetch_object($result)) {
+    while ($arquivo  = $stmt->fetchAll(PDO::FETCH_OBJ)) {
         print "<tr>
-                <td>".$arquivo->CODIGO."</td>
-                <td>".$arquivo->COD_CLIENTE."</td>
-                <td>".$arquivo->COD_CAVALO."</td>
-                <td>".$arquivo->COD_REBOQUE."</td>
-                <td>".$arquivo->COD_REGIAO."</td>
+                <td>".$arquivo->LICENCA."</td>
+                <td>".$arquivo->NomeAbreviado."</td>
+                <td>".$arquivo->PLCA_CAVALO."</td>
+                <td>".$arquivo->PLACA_REBOQUE."</td>
+                <td>".$arquivo->SIGLA_REGIAO."</td>
                 <td>
                     <a href='visualizar_arquivo.php?acao=VISUALIZAR&id_arquivo=".$arquivo->CODIGO."' target='_blank'><img src='IMG/pdf.png' style='width:30px; height:30px;cursor:pointer;'></a>
                     <a href='visualizar_arquivo.php?acao=DOWNLOAD&id_arquivo=".$arquivo->CODIGO."' target='_blank'><img src='IMG/download.png' style='width:30px; height:30px;cursor:pointer;'></a>
