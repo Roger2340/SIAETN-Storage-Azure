a9d3d8052bea817b0e5f2035aecae2b7abcd59e6
Rogério Marques Araujo <44317683+Roger2340@users.noreply.github.com>
2018-12-28 22:25:41 -0200

    Create File 201812282225

---
+++ conexao_sqlserver.php
@@ -0,0 +1,14 @@
+<?php
+
+$connectionInfo = array("UID" => "ra805847@databasesiaetn", "pwd" => "ra3025@7610", "Database" => "sql_SIAETN", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:databasesiaetn.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+
+if($conn){
+    echo 'Conexao estabelecida';
+} else {
+    echo 'Conexao Falhou <br />';
+    die(print_r(sqlsrv_erros(),TRUE));
+}
+
+?>
